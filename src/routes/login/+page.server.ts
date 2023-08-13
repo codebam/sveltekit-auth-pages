436d8e332632c2d9a20baabe783378abed0260e6
Sean Behan <codebam@riseup.net>
2023-08-13 16:41:23 -0400

    update

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -3,8 +3,25 @@
 
 export const actions = {
 	login: async (event: any) => {
-		// TODO log the user in
-		console.log(event);
+		const result = await event.request.formData();
+		const username = result.get('username');
+		const password = result.get('password');
+		const hash = await event.platform?.env.SVELTE_DB.prepare(
+			'SELECT password FROM Users WHERE User = ?'
+		)
+			.bind(username)
+			.all();
+		const test_hash = Array.from(
+			new Uint8Array(
+				await crypto.subtle.digest('SHA-512', new TextEncoder().encode(String(password)))
+			)
+		)
+			.map((b) => b.toString(16).padStart(2, '0'))
+			.join('');
+		if (hash === test_hash) {
+			const cookie = event.cookies.set('cookie', test_hash, { path: '/' });
+			console.log(event);
+		}
 	},
 	register: async (event: any) => {
 		event.request.formData().then(async (result: FormData) => {
@@ -17,6 +34,7 @@ export const actions = {
 			)
 				.map((b) => b.toString(16).padStart(2, '0'))
 				.join('');
+			console.log(event.platform);
 			await event.platform?.env.SVELTE_DB.prepare('INSERT INTO Users VALUES (?, ?)')
 				.bind(username, hash)
 				.all();
